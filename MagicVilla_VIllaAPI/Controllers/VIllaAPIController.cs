c7dae2f46b78ae2ecf4c71f66f710ff39ff88912
Dilanka Yasith <dilanka.pemarathne@nobletec.co>
2024-02-21 14:20:52 +0530

    Get method

---
+++ MagicVilla_VIllaAPI/Controllers/VIllaAPIController.cs
@@ -0,0 +1,21 @@
+﻿using MagicVilla_VIllaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VIllaAPI.Controllers
+{
+    // [Route("api/[controller]")]
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VIllaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa{Id=1,Name="Pool View"},
+                new Villa{Id=2,Name="Beach View"}
+            };
+        }
+    }
+}
